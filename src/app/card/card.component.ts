3a330ea6248cb492b67600b83ac8c777234ebbf6
Stanislav Fesiuk <fesiuk.stanislaw@gmail.com>
2021-11-10 20:00:05 +0200

    commit

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,66 @@
+import {Component, OnInit} from '@angular/core';
+import {state, style, transition, trigger, animate} from "@angular/animations";
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css'],
+  animations: [
+    trigger('expandedPanel', [
+      state('clicked', style({ boxShadow: '0 0 5px rgb(12, 136, 243, 0.5)' })),
+      transition('initial <=> expanded', animate('0.8s')),
+    ]),
+  ],
+})
+export class CardComponent implements OnInit {
+  cards: number[] = [];
+
+  prevValue = [];
+
+  isExpanded: boolean = false
+  state: string[] = ['initial']
+
+  constructor() { }
+
+  ngOnInit(): void {
+    for ( let i = 0; i < 15; i++){
+
+      let a;
+
+      do{
+        a = this.generateNumber();
+      }
+      while (this.cards.indexOf(a) !== -1);
+
+      this.cards.push(a);
+    }
+    this.shuffleAndSum(this.cards);
+  }
+
+  expand(index:any) {
+    this.state[index] = 'clicked';
+  }
+
+  shuffleAndSum(arr: number[]){
+    this.cards.push.apply(arr, arr);
+  }
+
+  generateNumber(){
+    return Math.floor(Math.random() * 39);
+  }
+
+  click(event: any){ //Тут повина була бути логіка відгадування карток
+    let clickValue = event.target.innerText;
+    if(clickValue == this.prevValue){ //
+      let  al = setTimeout(function(){
+        classCheck.add('clicked');
+      },1500);
+    }
+    this.prevValue = event;
+    let classCheck = event.target.classList;
+    let  al = setTimeout(function(){
+      classCheck.add('clicked');
+    },250);
+  }
+
+}
